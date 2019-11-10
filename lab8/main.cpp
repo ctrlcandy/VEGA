5bf558fd0d23aabfb886994cf52157d80d60cc2e
ctrlcandy <alexandraleshanu@yandex.ru>
2019-11-10 23:32:37 +0300

    lab8 is completed

---
+++ lab8/main.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+#include "vector.h"
+
+
+int main()
+{
+	std::cout << "append: " << std::endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		append(i);
+
+		std::cout << "value: " << getValue(i)
+			<< " size: " << getSize()
+			<< " capacity: " << getCapacity() << std::endl;
+	}
+
+	int pos = 3, val = 15;
+
+	std::cout << "insert(" << pos << ", " << val << "):\n";
+	insert(pos, val);
+	print();
+
+	std::cout << "erase(" << pos << "):\n";
+	erase(pos);
+	print();
+
+	int newCap = 20;
+	std::cout << "capacity: " << getCapacity() << std::endl;
+	reserve(newCap);
+	std::cout << "reserve(" << newCap << "): "
+		<< "capacity: " << getCapacity() << std::endl;
+
+	std::cout << "squeeze: ";
+	squeeze();
+	std::cout << "capacity: " << getCapacity() << std::endl;
+
+
+	return 0;
+}
+
+// Вывод:
+// 
+// append: 
+// value: 0 size: 1 capacity: 1
+// value: 1 size: 2 capacity: 2
+// value: 2 size: 3 capacity: 4
+// value: 3 size: 4 capacity: 4
+// value: 4 size: 5 capacity: 8
+// value: 5 size: 6 capacity: 8
+// value: 6 size: 7 capacity: 8
+// value: 7 size: 8 capacity: 8
+// value: 8 size: 9 capacity: 16
+// value: 9 size: 10 capacity: 16
+// insert(3, 15):
+// 0 1 2 15 3 4 5 6 7 8 9 
+// erase(3):
+// 0 1 2 3 4 5 6 7 8 9 
+// capacity: 16
+// reserve(20): capacity: 32
+// squeeze: capacity: 10
---
+++ lab8/vector.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+#include <cassert>
+
+#include "vector.h"
+
+
+static int* data = nullptr;
+static size_t size = 0;
+static size_t capacity = 0;
+
+void reserve(size_t newCap)
+{
+	if (newCap <= capacity)
+		return;
+
+	capacity = (capacity == 0) ? 1 : capacity;
+	while (capacity < newCap) {
+		capacity *= 2;
+	}
+
+	if (data == nullptr)
+		data = new int[capacity];
+	else
+	{
+		int* newData = new int[capacity];
+		memcpy(newData, data, size * sizeof(int));
+		data = newData;
+	}
+}
+
+int getValue(size_t index)
+{
+	assert(index < size);
+	return data[index];
+}
+
+size_t getSize()
+{
+	return size;
+}
+
+size_t getCapacity()
+{
+	return capacity;
+}
+
+ void append(int value)
+{
+	 reserve(size + 1);
+
+	 data[size] = value;
+	 size++;
+} 
+
+void append(int* array, size_t count)
+{
+	for (size_t i = 0; i < count; i++)
+		append(array[i]);
+}
+
+void insert(size_t index, int value)
+{
+	reserve(size + 1);
+
+	for (size_t i = size; i > index; i--)
+		data[i] = data[i - 1];
+
+	data[index] = value;
+}
+
+void erase(size_t index)
+{
+	for (size_t i = index; i < size; i++)
+		data[i] = data[i + 1];
+}
+
+ int indexOf(int value)
+{
+	int index = -1;
+	for (size_t i = 0; i < size; i++) {
+		if (data[i] == value)
+			return i;
+	}
+	return -1;
+} 
+
+void squeeze()
+{ 
+	if (size == 0)
+		delete[] data;
+	else
+	{
+		int* newData = new int[size];
+		memcpy(newData, data, size * sizeof(int));
+		data = newData;
+		capacity = size;
+	}
+}
+
+void clear()
+{
+	for (size_t i = 0; i < size; i++)
+		data[i] = 0;
+
+	size = 0;
+}
+
+void print()
+{
+	for (size_t i = 0; i < size - 1; i++)
+		std::cout << getValue(i) << ", ";
+
+	std::cout << getValue(size - 1);
+}
