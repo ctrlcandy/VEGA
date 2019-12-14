9dd24997b23c410411c7fccc56f6b191e7f44f61
ctrlcandy <alexandraleshanu@yandex.ru>
2019-12-14 14:39:30 +0300

    lab14 is completed

---
+++ lab14/fileManager.h
@@ -0,0 +1,33 @@
+﻿#ifndef FILEMANAGER_H
+#define FILEMANAGER_H
+
+#include <fstream>
+
+/**
+* Вывести файл построчно, указывая номер строки.
+*/
+void filePrint(std::fstream& file);
+
+/**
+* Добавить указанную строку в конец файла.
+*/
+void fileAppend(std::fstream& file, char* str);
+
+/**
+* Вывести строки с их номерами, в которых
+* есть указанная подстрока; (указанную подстроку в строке
+* выводить в ВЕРХНЕМ регистре. (не сделаль))
+*/
+void fileSearch(std::fstream& file, char* needle);
+
+/**
+* Перезаписать файл, удалив строку с указанным номером.
+*/
+void fileRemoveString(std::fstream& file, char* name, size_t lineno);
+
+/**
+* Перезаписать файл, вставив указанную подстроку перед строкой с указанным номером.
+*/
+void fileInsertString(std::fstream& file, char* name, char* insert, size_t lineno);
+
+#endif // FILEMANAGER_H
---
+++ lab14/main.cpp
@@ -0,0 +1,40 @@
+﻿#include "fileManager.h"
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+	char name[260];
+	std::cout << "enter file name" << std::endl;
+	std::cin >> name;
+
+	std::fstream file(name, std::ios::in | std::ios::out | std::ios::app);
+	filePrint(file);
+	
+	std::cout << "file append" << std::endl;
+	char str[50];
+	std::cin.ignore();
+	std::cin.getline(str, 50);
+	fileAppend(file, str);
+	filePrint(file); 
+	
+	std::cout << "file search" << std::endl;
+	char needle[50];
+	std::cin.ignore();
+	std::cin.getline(needle, 50);
+	fileSearch(file, needle); 
+
+	std::cout << "file remove string" << std::endl;
+	fileRemoveString(file, name, 6);
+	filePrint(file); 
+
+	std::cout << "file insert string" << std::endl;
+	char insert[50];
+	std::cin.getline(insert, 50);
+	fileInsertString(file, name, insert, 6);
+	filePrint(file);
+
+	file.close();
+	return 0;
+}
